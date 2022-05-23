f0ed9a8aa38e69b0bfa6fbbbf684e9d9d50a1d64
PJSanto429 <86988750+PJSanto429@users.noreply.github.com>
2022-05-22 22:14:24 -0400

    5/22/22 - 10:14 PM

--- testBed.py
+++ testBed.py
@@ -10,14 +10,6 @@ from loading import loading
 from typeEffect import *
 from datetime import datetime
 
-'''
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S") #https://www.programiz.com/python-programming/datetime/current-time
-print(current_time)'''
-
-#saveGame('start')
-#loadGame()
-
 player.room = 'first'
 text_input('look', player.room)
 while True: #this is a very simple interactive 'window' that allows for text input
