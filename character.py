46a89a7471e16ae5b4d6fce0d8ee7dc6453aa698
PJSanto429 <86988750+PJSanto429@users.noreply.github.com>
2022-05-27 08:49:23 -0400

    removed files

--- character.py
+++
@@ -1,7 +0,0 @@
-#character class
-
-class Character():
-    def __init__(self, name = 'none', kind = 'none', room = 'none'):
-        self.name = name
-        self.kind = kind #different kids = enemy, shopkeep, thief, healer, etc.
-        self.room = room #where they start(some will be able to move)
--- firstStartup.py
+++
@@ -1,7 +0,0 @@
-#this runs a simple command that creates a new folder for all save states to go into
-def startup():
-    from os import mkdir
-    try:
-        mkdir('players') #don't change this or save/load game will not work properly
-    except:
-        pass
